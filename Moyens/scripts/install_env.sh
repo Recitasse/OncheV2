ff68280ca17eca0ad97f0f2bd734b82a5b41d36e
Recitasse <raphael.gougeonpro@gmail.com>
2024-03-16 16:01:13 +0100

    maj

--- Moyens/scripts/install_env.sh
+++ Moyens/scripts/install_env.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+sudo apt update
+
+# python part
+sudo apt install python3-venv libdbus-glib-1-dev
+pip install secretstorage dbus-python
+
+python3 -venv venvOnche
+pip install -r ../../../requirements.txt
